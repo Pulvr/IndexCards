ca2ca7ba4181f7d54fa1e887a1e6ce499b1381a7
Pulvr <pulv3rtoastmann@gmail.com>
2024-05-02 12:06:01 +0200

    first commit

---
+++ src/test/java/de/indexcards/indexcards/IndexcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.indexcards.indexcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
